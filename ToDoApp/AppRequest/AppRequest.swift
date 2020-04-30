03c5ac5fedc7d7587fa4f2ce224caf44c12aca52
fummicc1 <fumiya.tennis1@gmail.com>
2020-04-30 11:42:35 +0900

    clean: AppRequest.buildURLRequest

--- ToDoApp/AppRequest/AppRequest.swift
+++ ToDoApp/AppRequest/AppRequest.swift
@@ -12,6 +12,14 @@ import RxRelay
 
 public class AppRequest {
     
+    struct URLRequestBuilder {
+        let paths: [String]
+        let parameters: [String: String]
+        let headers: [String: String]
+        let method: String
+        let body: Data?
+    }
+    
     public enum Error: Swift.Error {
         case some(Swift.Error)
         case invalidPaths
@@ -29,55 +37,102 @@ public class AppRequest {
     
     private let baseURL: String = "http://localhost:8080"
     
-    public func get<Entity: Codable>(paths: [String], paramters: [String: String] = [:], headerFields: [String: String] = [:]) -> Single<Entity> {
+    public func get<Entity: Codable>(paths: [String], paramters: [String: String] = [:], headers: [String: String] = [:]) -> Single<Entity> {
         Single.create { [weak self] observer-> Disposable in
             guard let self = self else {
                 return Disposables.create()
             }
-            guard var baseURL = URL(string: self.baseURL) else {
-                fatalError()
-            }
-            paths.forEach { baseURL.appendPathComponent($0) }
-            guard var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true) else {
-                observer(.error(Error.invalidPaths))
-                return Disposables.create()
-            }
-            components.queryItems = []
-            for (name, value) in paramters {
-                let queryItem = URLQueryItem(name: name, value: value)
-                components.queryItems?.append(queryItem)
-            }
-            guard let url = components.url else {
-                observer(.error(Error.invalidParameters))
-                return Disposables.create()
-            }
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            for (field, value) in headerFields {
-                request.addValue(value, forHTTPHeaderField: field)
-            }
-            self.session.dataTask(with: request, completionHandler: { data, response, error in
-                if let error = error {
-                    observer(.error(Error.some(error)))
-                    return
-                }
-                if let response = response as? HTTPURLResponse {
-                    if !(200..<300).contains(response.statusCode) {
-                        observer(.error(Error.unsuccessfulStatusCode(response.statusCode)))
+            do {
+                let request = try self.buildURLRequest(URLRequestBuilder(paths: paths, parameters: paramters, headers: headers, method: "GET", body: nil))
+                self.session.dataTask(with: request, completionHandler: { data, response, error in
+                    if let error = error {
+                        observer(.error(Error.some(error)))
                         return
                     }
-                }
-                guard let data = data else {
-                    observer(.error(Error.noData))
-                    return
-                }
-                guard let entity = try? JSONDecoder().decode(Entity.self, from: data) else {
-                    observer(.error(Error.failedToDecode(type: Entity.self, data: data)))
-                    return
-                }
-                observer(.success(entity))
-            })
+                    if let response = response as? HTTPURLResponse {
+                        if !(200..<300).contains(response.statusCode) {
+                            observer(.error(Error.unsuccessfulStatusCode(response.statusCode)))
+                            return
+                        }
+                    }
+                    guard let data = data else {
+                        observer(.error(Error.noData))
+                        return
+                    }
+                    guard let entity = try? JSONDecoder().decode(Entity.self, from: data) else {
+                        observer(.error(Error.failedToDecode(type: Entity.self, data: data)))
+                        return
+                    }
+                    observer(.success(entity))
+                })
+            } catch let error as AppRequest.Error {
+                observer(.error(error))
+            } catch {
+                observer(.error(Error.some(error)))
+            }
             return Disposables.create()
         }
+    }
+    
+    public func create<Entity: Codable>(paths: [String], body: Data, headers: [String: String] = [:]) -> Single<Entity> {
+        Single.create { [weak self] observer -> Disposable in
+            guard let self = self else {
+                return Disposables.create()
+            }
+            do {
+                let request = try self.buildURLRequest(URLRequestBuilder(paths: paths, parameters: [:], headers: headers, method: "POST", body: body))
+                self.session.dataTask(with: request, completionHandler: { data, response, error in
+                    if let error = error {
+                        observer(.error(Error.some(error)))
+                        return
+                    }
+                    if let response = response as? HTTPURLResponse {
+                        if !(200..<300).contains(response.statusCode) {
+                            observer(.error(Error.unsuccessfulStatusCode(response.statusCode)))
+                            return
+                        }
+                    }
+                    guard let data = data else {
+                        observer(.error(Error.noData))
+                        return
+                    }
+                    guard let entity = try? JSONDecoder().decode(Entity.self, from: data) else {
+                        observer(.error(Error.failedToDecode(type: Entity.self, data: data)))
+                        return
+                    }
+                    observer(.success(entity))
+                })
+            } catch let error as AppRequest.Error {
+                observer(.error(error))
+            } catch {
+                observer(.error(Error.some(error)))
+            }
+            return Disposables.create()
+        }
+    }
+    
+    private func buildURLRequest(_ buiilder: URLRequestBuilder) throws -> URLRequest {
+        guard var baseURL = URL(string: self.baseURL) else {
+            fatalError()
+        }
+        buiilder.paths.forEach { baseURL.appendPathComponent($0) }
+        guard var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true) else {
+            throw Error.invalidPaths
+        }
+        components.queryItems = []
+        for (name, value) in buiilder.parameters {
+            let queryItem = URLQueryItem(name: name, value: value)
+            components.queryItems?.append(queryItem)
+        }
+        guard let url = components.url else {
+            throw Error.invalidParameters
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = buiilder.method
+        request.httpBody = buiilder.body
+        for (field, value) in buiilder.headers {
+            request.addValue(value, forHTTPHeaderField: field)
+        }
+        return request
     }
 }
